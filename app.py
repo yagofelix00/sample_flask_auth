90af48fabafe9447e6b410cb61755a3bb13881b1
Yago Felix <yagofelix007@gmail.com>
2025-10-26 23:59:00 -0300

    feat: add POST /login route and setup SQLAlchemy database connection

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,6 +6,18 @@ app.config['SECRET_KEY'] = 'your secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db = SQLAlchemy(app)
+
+@app.route('/login', methods="POST")
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        # Login
+        pass
+
+    return jsonify({"message": "Credenciais Inválidas"}), 400
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
