a768dcc03c205524dfb5fe6d252eacb298061655
Yago Felix <yagofelix007@gmail.com>
2025-10-27 01:49:49 -0300

    feat: setup database structure and integrate SQLAlchemy with app

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from models.user import User
+from database import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-db = SQLAlchemy(app)
-
+db.init_app(app)
 @app.route('/login', methods="POST")
 def login():
-    data = request.json
+    data = request.json 
     username = data.get("username")
     password = data.get("password")
 
@@ -18,6 +18,7 @@ def login():
         pass
 
     return jsonify({"message": "Credenciais Inválidas"}), 400
+
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
