c050ea8c34bfe4b0bd8b1fbdb37d04f8ac4765e3
Yago Felix <yagofelix007@gmail.com>
2025-10-28 23:57:09 -0300

    feat(auth): integrate Flask-Login with user session management
    - Added LoginManager setup and user_loader callback
    - Implemented user authentication using login_user() and current_user
    - Updated /login route to handle authenticated sessions

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your secret_key'
@@ -10,8 +10,13 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 Login_manager = LoginManager()
 db.init_app(app)
 Login_manager.init_app(app)
+Login_manager.login_view = 'Login'
 
-@app.route('/login', methods="POST")
+@Login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id)
+
+@app.route('/login', methods=["POST"])
 def login():
     data = request.json 
     username = data.get("username")
@@ -22,7 +27,9 @@ def login():
         user = User.query.filter_by(username=username).first()
 
         if user and user.password == password:
-                return jsonify({"message": "Autentificação realizadfa com sucesso!"}), 200
+                login_user(user)
+                print(current_user.is_authenticated)
+                return jsonify({"message": "Autentificação realizada com sucesso!"}), 200
 
     return jsonify({"message": "Credenciais Inválidas"}), 400
 
